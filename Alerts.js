634b2ea23c5d7b61bf3c46a98f5cc50287f97ae3
Chase DeMaster <chasedemaster.tech@gmail.com>
2022-07-09 14:26:02 -0500

    Delete Alerts.js

--- Alerts.js
+++
@@ -1,8 +0,0 @@
-// every blank number of clicks we get an alert and draw an id randomly from the API and will have a result on the time allocation
-import { React, useState, useEffect } from "react";
-
-function Alert() {
-  return <></>;
-}
-
-export default Alert;
