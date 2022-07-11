a15b553eba3fe76ec6e8d7859a8881d37ed36770
Jamie <bennythedev@gmail.com>
2022-07-11 09:14:07 -0500

    moved db.json

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,24 @@
+import { dblClick } from '@testing-library/user-event/dist/click';
+import { useEffect } from 'react';
 import './App.css';
+import Header from './Header';
+import table from './table1.jpeg';
+import FarmData from './db.json';
 
 function App() {
+
+function fetchFarm() {
+  fetch('./db.json')
+  .then((r) => r.json())
+  .then((farmData) =>console.log(farmData))
+}
+
+useEffect(fetchFarm, [])
+ 
   return (
     <div className="App">
-      <p>Farmville</p>
+      <Header />
+      <img src={table}></img>
     </div>
   );
 }
