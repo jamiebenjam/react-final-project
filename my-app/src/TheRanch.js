d43bcd1a44f97831073cd24775f23c1e59797b15
Jamie <bennythedev@gmail.com>
2022-07-14 11:38:28 -0500

    styling form

--- my-app/src/TheRanch.js
+++ my-app/src/TheRanch.js
@@ -17,14 +17,14 @@ function TheRanch ( {bank, setBank, setSpeed, speed}) {
             <img src={ranch}></img>
             <h1>Debt to Bank: {remainder > 0 ? new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD' }).format(remainder) : alert(`You saved Codee Ranch and restored the family name!!!!! You green thumb, you!`)}</h1>
             <h1>Bank Account: {new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD' }).format(bank)}</h1>
-            <p>You've inherited a ranch from your Grandpa Codee! Unfortunately, it's in shambles and it's YOUR job to bring glory back to the family name. You have $10 to start, the entirety of your material possessions, and 150 days left in the growing season to get out of debt before the bank seizes your ranch. Choose what speed you play in.</p>
-            <label htmlFor="type">Speed: </label>
+            <label chtmlFor="type">Speed: </label>
             <select onChange={handleSpeedChange} id="type" name="type" >
                 <option value={speed}>Select One</option>
                 <option value="10000">Slow</option>
                 <option value="5000">Medium</option>
                 <option value="1000">Fast</option>
             </select>
+            <p className='copy'>You've inherited a ranch from your Grandpa Codee! Unfortunately, it's in shambles and it's YOUR job to bring glory back to the family name. You have $10 to start, the entirety of your material possessions, and 150 days left in the growing season to get out of debt before the bank seizes your ranch. Choose what speed you play in.</p>
         </div>
    
     )
